14bc9682c67556b2233944f120c5d2e8a82e3fef
YimJet <mingg@mingworld>
2018-09-17 10:43:45 +0700

    สร้าง WordCard.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <CharacterCard value = "h"/>
-          <CharacterCard value = 'i'/>
+        { 
+          <WordCard value = "Hello"/>
+        }
       </div>
     );
   }
